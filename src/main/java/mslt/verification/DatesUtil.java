2114b53989287c4d8ce5a8302b673a963e0d9db9
karthikeyan.ks <karthikeyan.ks@indpro.se>
2014-05-05 12:47:48 -0500

    Initial commit for the api epg Guide with complete coverage - 7 days x 98 channels = 686 scenarios, associated util files ChannelsUtil, DatesUtil

---
+++ src/main/java/mslt/verification/DatesUtil.java
@@ -0,0 +1,27 @@
+package mslt.verification;
+
+public class DatesUtil {
+	private String id;
+	private String date;
+	private String displayName;
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+}
+
